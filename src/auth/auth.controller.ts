31a7ada6e6ee8cb546a5524aae7209834d8e59c2
oBonn14 <exsan1515@gmail.com>
2026-08-16 10:59:09 +0700

    FIX :: api upload photo profile

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -76,7 +76,7 @@ export class AuthController {
   @UseInterceptors(
     FileInterceptor("avatar", {
       storage: avatarMulterStorage(),
-      limits: { fileSize: 2 * 1024 * 1024 },
+      limits: { fileSize: 5 * 1024 * 1024 },
     }),
   )
   updateMe(
